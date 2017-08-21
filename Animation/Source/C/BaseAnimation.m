4a6ef42793956ea1dc4ed9da570d993b42241a51
Li_guang <liliguang@LLGMBP.local>
2017-08-21 19:11:44 +0800

    关于CABaseAnimation动画基础的内容

---
+++ Animation/Source/C/BaseAnimation.m
@@ -0,0 +1,244 @@
+//
+//  BaseAnimation.m
+//  Animation
+//
+//  Created by 李礼光 on 2017/8/21.
+//  Copyright © 2017年 LG. All rights reserved.
+//
+
+#import "BaseAnimation.h"
+
+@interface BaseAnimation ()<CAAnimationDelegate>
+@property (nonatomic, strong) CAShapeLayer *layer1;
+@property (nonatomic, strong) CAShapeLayer *layer2;
+@property (nonatomic, strong) CABasicAnimation *anim;
+@end
+
+@implementation BaseAnimation
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+    self.title = @"Line Animation";
+    
+    [self addLine1];
+    [self addLine2];
+    
+    
+}
+
+#pragma mark - UI
+- (void)addLine1 {
+    UIBezierPath *line = [UIBezierPath bezierPath];
+    [line moveToPoint:CGPointMake(100, 200)];
+    [line addLineToPoint:CGPointMake(100, 400)];
+    
+    _layer1 = [CAShapeLayer layer];
+    _layer1.frame = CGRectMake(0, 64, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height - 64);
+    _layer1.path = line.CGPath;
+    _layer1.lineWidth = 15;
+    _layer1.fillColor = [UIColor clearColor].CGColor;
+    _layer1.strokeColor = [UIColor blueColor].CGColor;
+    _layer1.lineCap = kCALineCapRound;
+    _layer1.lineJoin = kCALineJoinRound;
+    _layer1.backgroundColor = [UIColor colorWithWhite:0.5 alpha:0.5].CGColor;
+    [self.view.layer addSublayer:_layer1];
+}
+
+- (void)addLine2 {
+    UIBezierPath *line = [UIBezierPath bezierPath];
+    [line moveToPoint:CGPointMake(300, 200)];
+    [line addLineToPoint:CGPointMake(300, 400)];
+    
+    _layer2 = [CAShapeLayer layer];
+    _layer2.frame = CGRectMake(0, 64, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height - 64);
+    _layer2.path = line.CGPath;
+    _layer2.lineWidth = 15;
+    _layer2.fillColor = [UIColor clearColor].CGColor;
+    _layer2.strokeColor = [UIColor redColor].CGColor;
+    _layer2.lineCap = kCALineCapRound;
+    _layer2.lineJoin = kCALineJoinRound;
+    [self.view.layer addSublayer:_layer2];
+}
+
+#pragma mark - 添加动画
+- (CABasicAnimation *)addAnimation:(CAShapeLayer *)layer
+                           keyPath:(NSString *)keyPath
+                              From:(id)fromValue
+                                To:(id)toValue
+                          Duration:(NSTimeInterval)duration
+                          FillMode:(NSString *)fillMode
+                          Delegate:(id)delegate {
+    
+    
+    CABasicAnimation *animation = [CABasicAnimation animation];
+    animation.keyPath = keyPath;
+    animation.fromValue = fromValue;
+    animation.toValue = toValue;
+    animation.duration = duration;
+    animation.fillMode = fillMode;
+    animation.delegate = delegate;
+    animation.removedOnCompletion = YES;
+    [layer addAnimation:animation forKey:nil];
+    return animation;
+}
+
+//颜色从有到无
+- (void)lineStrokeStart {
+    [self addAnimation:_layer1 keyPath:@"strokeStart" From:@0 To:@1 Duration:1 FillMode:kCAFillModeForwards Delegate:self];
+    [self addAnimation:_layer2 keyPath:@"strokeStart" From:@0 To:@1 Duration:1 FillMode:kCAFillModeForwards Delegate:self];
+}
+
+//颜色从无到有
+- (void)lineStrokeEnd {
+    [self addAnimation:_layer1 keyPath:@"strokeEnd" From:@0 To:@1 Duration:1 FillMode:kCAFillModeForwards Delegate:self];
+    [self addAnimation:_layer2 keyPath:@"strokeEnd" From:@0 To:@1 Duration:1 FillMode:kCAFillModeForwards Delegate:self];
+}
+
+//透明度
+- (void)lineOpacity {
+    [self addAnimation:_layer1 keyPath:@"opacity" From:@0 To:@1 Duration:1 FillMode:kCAFillModeForwards Delegate:self];
+    [self addAnimation:_layer2 keyPath:@"opacity" From:@1 To:@0 Duration:1 FillMode:kCAFillModeForwards Delegate:self];
+}
+
+//位置
+- (void)positionX {
+    [self addAnimation:_layer1 keyPath:@"position.x"
+                  From:[NSValue valueWithCGPoint:CGPointMake(100, 0)]
+                    To:[NSValue valueWithCGPoint:CGPointMake(400, 0)]
+              Duration:1
+              FillMode:kCAFillModeForwards
+              Delegate:self];
+    
+    [self addAnimation:_layer2 keyPath:@"position.x"
+                  From:[NSValue valueWithCGPoint:CGPointMake(100, 0)]
+                    To:[NSValue valueWithCGPoint:CGPointMake(400, 0)]
+              Duration:1
+              FillMode:kCAFillModeForwards
+              Delegate:self];
+    
+}
+- (void)positionY {
+    [self addAnimation:_layer1 keyPath:@"position.y"
+                  From:[NSValue valueWithCGPoint:CGPointMake(350, 0)]
+                    To:[NSValue valueWithCGPoint:CGPointMake(350, 0)]
+              Duration:1
+              FillMode:kCAFillModeForwards
+              Delegate:self];
+    
+    [self addAnimation:_layer2 keyPath:@"position.y"
+                  From:[NSValue valueWithCGPoint:CGPointMake(350, 0)]
+                    To:[NSValue valueWithCGPoint:CGPointMake(350, 0)]
+              Duration:1
+              FillMode:kCAFillModeForwards
+              Delegate:self];
+}
+
+
+- (void)scaleX {
+    [self addAnimation:_layer1 keyPath:@"transform.scale.x" From:@1 To:@0.5 Duration:1 FillMode:kCAFillModeForwards Delegate:self];
+}
+- (void)scaleY {
+    [self addAnimation:_layer1 keyPath:@"transform.scale.y" From:@1 To:@0.5 Duration:1 FillMode:kCAFillModeForwards Delegate:self];
+}
+- (void)scaleZ {
+    [self addAnimation:_layer1 keyPath:@"transform.scale.z" From:@1 To:@0.5 Duration:1 FillMode:kCAFillModeForwards Delegate:self];
+}
+
+- (void)rotationX {
+    [self addAnimation:_layer1 keyPath:@"transform.rotation.x" From:@0 To:@M_PI_2 Duration:1 FillMode:kCAFillModeForwards Delegate:self];
+}
+- (void)rotationY {
+    [self addAnimation:_layer1 keyPath:@"transform.rotation.y" From:@0 To:@M_PI_2 Duration:1 FillMode:kCAFillModeForwards Delegate:self];
+}
+
+- (void)rotationZ {
+    [self addAnimation:_layer1 keyPath:@"transform.rotation.z" From:@0 To:@M_PI_2 Duration:1 FillMode:kCAFillModeForwards Delegate:self];
+}
+
+
+
+
+#pragma mark - 按钮方法
+
+- (IBAction)rotationX:(id)sender {
+    [self rotationX];
+}
+- (IBAction)rotationY:(id)sender {
+    [self rotationY];
+}
+- (IBAction)rotationZ:(id)sender {
+    [self rotationZ];
+}
+- (IBAction)scaleX:(id)sender {
+    [self scaleX];
+}
+- (IBAction)scaleY:(id)sender {
+    [self scaleY];
+}
+- (IBAction)scaleZ:(id)sender {
+    [self scaleZ];
+}
+- (IBAction)positionX:(id)sender {
+    [self positionX];
+}
+- (IBAction)positionY:(id)sender {
+    [self positionY];
+}
+- (IBAction)opacity:(id)sender {
+    [self lineOpacity];
+}
+- (IBAction)strokeStart:(id)sender {
+    [self lineStrokeStart];
+}
+- (IBAction)strokeEnd:(id)sender {
+    [self lineStrokeEnd];
+}
+
+#pragma mark - delegate
+- (void)animationDidStart:(CAAnimation *)anim {
+    NSLog(@"动画开始");
+}
+
+- (void)animationDidStop:(CAAnimation *)anim finished:(BOOL)flag {
+    NSLog(@"动画结束");
+    
+}
+
+
+
+
+
+
+
+@end
+
+
+
+/***
+ 
+ 附录 :
+ transform.rotation：旋转动画。
+ transform.rotation.x：按x轴旋转动画。
+ transform.rotation.y：按y轴旋转动画。
+ transform.rotation.z：按z轴旋转动画。
+ transform.scale：按比例放大缩小动画。
+ transform.scale.x：在x轴按比例放大缩小动画。
+ transform.scale.y：在y轴按比例放大缩小动画。
+ transform.scale.z：在z轴按比例放大缩小动画。
+ position：移动位置动画。
+ opacity：透明度动画
+ strokeEnd :颜色从无到有
+ strokeStart :颜色从有到无
+ */
+
+
+
+
+
+
+
+
+
+
---
+++ Animation/Source/C/RootTableViewController.m
@@ -0,0 +1,67 @@
+//
+//  RootTableViewController.m
+//  Animation
+//
+//  Created by 李礼光 on 2017/8/21.
+//  Copyright © 2017年 LG. All rights reserved.
+//
+
+#import "RootTableViewController.h"
+#import "BaseAnimation.h"
+static NSString *cellIdentify = @"contentCell";
+
+@interface RootTableViewController ()
+@property (nonatomic, strong) NSArray *contentArr;
+@end
+
+@implementation RootTableViewController
+
+#pragma mark - 懒加载
+- (NSArray *)contentArr {
+    if (_contentArr ==nil) {
+        _contentArr = [NSArray arrayWithObjects:[BaseAnimation class], nil];
+    }
+    return _contentArr;
+}
+
+
+#pragma mark - 系统方法
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor whiteColor];
+    self.title = @"Animation";
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:cellIdentify];
+    
+    //● 多出利用绘图技术，采用贝塞尔曲线进行界面绘制。
+    //● 合理利用 CAGradientLayer、CAShapeLayer、CAKeyframeAnimation 进行折线图、曲线图的绘制。
+
+}
+
+
+
+#pragma mark - 代理方法
+#pragma mark - Table view data source
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.contentArr.count;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentify forIndexPath:indexPath];
+    
+    cell.textLabel.text = NSStringFromClass(self.contentArr[indexPath.row]);
+    
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [self.navigationController pushViewController:[[self.contentArr[indexPath.row] alloc] init]
+                                         animated:YES];
+}
+
+@end
